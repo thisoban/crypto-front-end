d3c3da293974ebc83b3db962424023710ab89438
thisoban <thisoban_1995@hotmail.com>
2022-03-24 11:10:01 +0100

    menu and page added

--- crypto-front-end/src/App.vue
+++ crypto-front-end/src/App.vue
@@ -5,21 +5,14 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+      <nav class="navbar">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/user"> User</RouterLink>
+      </nav>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
