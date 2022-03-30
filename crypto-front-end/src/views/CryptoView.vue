38c0e56b4938a28a39b190a40b82e35bcc4c9451
thisoban <thisoban_1995@hotmail.com>
2022-03-30 18:34:42 +0200

    tweaked coding

---
+++ crypto-front-end/src/views/CryptoView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an crypto page</h1>
+
+    <div class="cryptolistings">
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+9 
