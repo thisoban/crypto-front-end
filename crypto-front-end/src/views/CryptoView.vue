9896830d5d45c2e96f4c31b07bcc859aef75c5f2
thisoban <thisoban_1995@hotmail.com>
2022-03-31 13:51:22 +0200

    created crypto page, made table need some tweaks

--- crypto-front-end/src/views/CryptoView.vue
+++ crypto-front-end/src/views/CryptoView.vue
@@ -3,6 +3,19 @@
     <h1>This is an crypto page</h1>
 
     <div class="cryptolistings">
+      <table>
+        <tr>
+            <th>ranking number </th>
+            <th>name </th>
+            <th> price </th>
+            <th>24% </th>
+            <th>7d% </th>
+            <th>market cap </th>
+            <th>volume(24h) </th>
+            <th>circulating supply </th>
+        </tr>
+        
+      </table>
     </div>
   </div>
 </template>
@@ -14,6 +27,15 @@
     display: flex;
     align-items: center;
   }
+}
+table{
+  width: 100%;
+}
+
+tr, th {
+  border: 1px solid white;
+  text-align: left;
+  padding: 8px;
 }
 </style>
 9 
