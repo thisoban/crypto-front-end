8d23ef78bc505f1c4ab7c3654335e03b7c8b129b
thisoban <thisoban_1995@hotmail.com>
2022-04-20 10:50:19 +0200

    codeing tweaked to show json value in table

--- crypto-front-end/src/components/CryptoComponentn.vue
+++ crypto-front-end/src/components/CryptoComponentn.vue
@@ -1,24 +1,27 @@
 <script> 
 import crypto from "../temp/crypto.json"
 
-export default{
- name: "crypto",
+  export default{
+  name: "crypto",
   data: () => {
     return {
       isHidden: false,
       data: crypto
     }
+  },
+  created() {
+    console.log(this.data);
   }
 }
 
 </script>
-<template>
-{{data}}
-  <tr v-for="(item,index) in data" :key="item.id">
-       <th scope="row">2</th>
-          <td>{{item[index]}}</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+<template >
+  <!-- {{data}}  -->
+  <tr v-for="(item, index) in data.data" :key="item.id">
+    <td><b>{{index + 1}}</b></td>
+    <td>{{item.id}}</td>  
+    <td>{{item.name}}</td> 
+    <td>{{item.symbol}}</td>
   </tr>
 </template>
 <style scoped>
